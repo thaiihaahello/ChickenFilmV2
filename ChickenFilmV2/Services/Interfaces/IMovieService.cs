e8e82e99b4e6ea39b251a839d352615e1fd5062e
hung <hungnvvde180650@fpt.edu.vn>
2025-04-24 08:14:13 +0700

    Add admin controller, services, and admin views

---
+++ ChickenFilmV2/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,11 @@
+﻿using ChickenFilmV2.Models;
+
+namespace ChickenFilmV2.Services.Interfaces;
+public interface IMovieService
+{
+    Task<IEnumerable<Movie>> GetAllMoviesAsync();
+    Task<Movie> GetMovieByIdAsync(int movieId);
+    Task AddMovieAsync(Movie movie);
+    Task UpdateMovieAsync(Movie movie);
+    Task DeleteMovieAsync(int movieId);
+}
---
+++ ChickenFilmV2/Services/Interfaces/ITheaterService.cs
@@ -0,0 +1,14 @@
+﻿using ChickenFilmV2.Models;
+
+namespace ChickenFilmV2.Services.Interfaces
+{
+    public interface ITheaterService
+    {
+        Task<IEnumerable<Theater>> GetAllTheatersAsync();
+        Task<Theater> GetTheaterByIdAsync(int theaterId);
+        Task AddTheaterAsync(Theater theater);
+        Task UpdateTheaterAsync(Theater theater);
+        Task DeleteTheaterAsync(int theaterId);
+    }
+
+}
